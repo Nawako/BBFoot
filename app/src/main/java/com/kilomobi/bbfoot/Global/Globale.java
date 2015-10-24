4f496057d656d210d813f5fcc5df6afd7b200cc3
Fabrice <nawako_68@hotmail.fr>
2015-10-24 21:11:47 +0200

    Création des constantes pour l'accès à l'API

---
+++ app/src/main/java/com/kilomobi/bbfoot/Global/Globale.java
@@ -0,0 +1,11 @@
+package com.kilomobi.bbfoot.Global;
+
+/**
+ * Created by Nawako on 24/10/2015.
+ */
+public class Globale {
+    public static String webURL ="http://bbfoot.azurewebsites.net/api/";
+    public static String DELETE = "suppression";
+    public static String CREATE = "creation";
+    public static String MODIFY = "modification";
+}
